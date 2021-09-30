3df1d6750cacbd652e28b2ddbabc877d6b3f8109
remosrulloda <remosrulloda@gmail.com>
2021-09-30 13:00:20 -0700

    Initial Commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,40 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+
+    public String body;
+    public String createdAt;
+    public long id;
+    public User user;
+
+    // Give in a JSON object that represent a tweet and turn it into a Java Tweet Object
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet(); // Build the tweet as per the fields
+        tweet.body = jsonObject.getString("text"); // from API
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.id = jsonObject.getLong("id");
+        // will take in a JSON object and returns the User model
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user")); // this refers to JSON object but we need a JAVA object
+
+        return tweet;
+
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++){
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
+
+    }
+
+
+}
